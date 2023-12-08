dadf64109996db80bf5b79119e5024a85371d07e
Reza Panahi <rezapanahiac1@gmail.com>
2023-12-08 16:38:51 +0330

    modify models and add migrations

--- main/models.py
+++ main/models.py
@@ -1,9 +1,31 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
-class Book(models.Model):
-    name = models.CharField(max_length=300)
-    author = models.CharField(max_length=100)
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    bookLandedNumber = models.IntegerField(
+        validators=[MaxValueValidator(2), MinValueValidator(0)]
+    )
 
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.CharField(max_length=100)
+    isRented = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Landing(models.Model):
+    date = models.DateField()
+    books = models.ManyToManyField(Book, related_name="landings")
+    client = models.ForeignKey(Client, on_delete=models.PROTECT, related_name="client")
+
+    def __str__(self):
+        return ""
